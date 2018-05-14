dfa3548df4214a767b951fc1bd76b5824ccc5115
Gavin Foster <gavinnfoster@gmail.com>
2018-05-14 01:43:36 -0500

    adding submit

--- components/GeneralForm.vue
+++ components/GeneralForm.vue
@@ -1,22 +1,22 @@
-<template>
-  <el-form ref="form" name="contact" action="" method="post" netlify :model="form" label-width="120px">
+<template ref="form">
+  <el-form ref="contactForm" name="contact" action="" method="post" netlify :model="form" label-width="120px">
     <input type="hidden" name="form-name" value="contact" />
     <header>
       <h5>We'd love to hear from you, so please contact us. Fill out the form with any information or questions and we will contact you as soon as possible.</h5>
     </header>
     <div class="form__row">
       <el-form-item label="Name">
-        <el-input v-model="name"></el-input>
+        <el-input v-model="form.name"></el-input>
       </el-form-item>
       <el-form-item label="Email">
-        <el-input v-model="email"></el-input>
+        <el-input v-model="form.email"></el-input>
       </el-form-item>
     </div>
     <div class="form__row">
       <el-form-item label="Type of Inquiry">
-        <el-select v-model="inquiryType" placeholder="Select">
+        <el-select v-model="form.inquiryType" placeholder="Select">
           <el-option
-            v-for="item in options"
+            v-for="item in form.options"
             :key="item.value"
             :label="item.label"
             :value="item.value">
@@ -24,10 +24,10 @@
         </el-select>
       </el-form-item>
     </div>
-    <div v-if="inquiryType === 'trip'" class="form__row">
+    <div v-if="form.inquiryType === 'trip'" class="form__row">
       <el-form-item style="min-width:413px" label="Date Range">
         <el-date-picker
-          v-model="date"
+          v-model="form.date"
           type="daterange"
           range-separator="To"
           start-placeholder="Start date"
@@ -40,8 +40,12 @@
         type="textarea"
         :rows="4"
         placeholder="Please input"
-        v-model="textarea">
+        v-model="form.textarea">
       </el-input>
+    </el-form-item>
+    <el-form-item>
+      <br/>
+      <button type="submit">Send</button>
     </el-form-item>
   </el-form>
 </template>
@@ -50,50 +54,47 @@
   export default {
     data() {
       return {
-        name: '',
-        email: '',
-        textarea: '',
-        options: [{
-          value: 'trip',
-          label: 'Trip'
-        }, {
-          value: 'general',
-          label: 'General'
-        }],
-        inquiryType: '',
-        date: '',
-        pickerOptions2: {
-          shortcuts: [{
-            text: 'Last week',
-            onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-              picker.$emit('pick', [start, end]);
-            }
+        form: {
+          name: '',
+          email: '',
+          textarea: '',
+          options: [{
+            value: 'trip',
+            label: 'Trip'
           }, {
-            text: 'Last month',
-            onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-              picker.$emit('pick', [start, end]);
-            }
-          }, {
-            text: 'Last 3 months',
-            onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-              picker.$emit('pick', [start, end]);
-            }
-          }]
-        },
-      }
-    },
-    methods: {
-      onSubmit() {
-        console.log('submit!');
+            value: 'general',
+            label: 'General'
+          }],
+          inquiryType: '',
+          date: '',
+          pickerOptions2: {
+            shortcuts: [{
+              text: 'Last week',
+              onClick(picker) {
+                const end = new Date();
+                const start = new Date();
+                start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+                picker.$emit('pick', [start, end]);
+              }
+            }, {
+              text: 'Last month',
+              onClick(picker) {
+                const end = new Date();
+                const start = new Date();
+                start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+                picker.$emit('pick', [start, end]);
+              }
+            }, {
+              text: 'Last 3 months',
+              onClick(picker) {
+                const end = new Date();
+                const start = new Date();
+                start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                picker.$emit('pick', [start, end]);
+              }
+            }]
+          },
+        }
       }
     }
   }
@@ -123,6 +124,15 @@
         display:block;
       }
     }
+  }
+  button, .el-button--primary {
+    color: #3f46ad;
+    background-color:#fff;
+    border: 3px solid #3f46ad;
+    border-radius:0px;
+    padding: .5rem 1rem;
+    font-size: .8rem;
+    cursor:pointer;
   }
   .el-date-label {
     color:#606266;
